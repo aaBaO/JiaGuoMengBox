c6f514be678c479b89180212cbeb814ade700108
aaBaO <abao883@gmail.com>
2019-10-10 14:27:12 +0800

    1.Add compute worker. 2.Show progress bar when computing combination.

--- pages/BestChoice/BestChoice.js
+++ pages/BestChoice/BestChoice.js
@@ -12,199 +12,69 @@ Page({
     data: {
         // 计算离线在线标记,默认计算在线
         online_flag: true,
-        //默认过滤离线buff
-        filter_buff_type: 3,
-        industry_buildings:[],
-        business_buildings:[],
-        residence_buildings:[],
-        allBaseIncome:{},
-        allPolicyBuff:{},
+        allBaseIncome: {},
+        allPolicyBuff: {},
 
         computing: false,
         compute_cost_time: 0,
-        current_count:0,
-        max_count:0,
+        current_count: 0,
+        max_count: 0,
     },
 
-    timerId:0,
+    compute_worker: "undefined",
     /**
      * Lifecycle function--Called when page load
      */
     onLoad: function (options) {
-        for(var i in FooData.buildings_list){
-            var building = FooData.buildings_list[i]
-            if(!self_configs.buildings[building.id].enabled){
-                continue
+        this.compute_worker = this.startWorker(this)
+        this.compute_worker.postMessage({
+            msg: "setData",
+            data: {
+                db: FooData,
+                self_configs: self_configs,
             }
-            if(building.type == 1){
-                this.data.industry_buildings.push(building)
-            }
-            if(building.type == 2){
-                this.data.business_buildings.push(building)
-            }
-            if(building.type == 3){
-                this.data.residence_buildings.push(building)
-            }
-        }
-
-        //计算所有建筑的基本收益
-        this.data.allBaseIncome = this.getAllBaseIncome()
-        //获得所有类型的政策buff加成
-        this.data.allPolicyBuff = this.getAllPolicyBuff()
-
-        this.setData({
-            allBaseIncome: this.data.allBaseIncome,
-            allPolicyBuff: this.data.allPolicyBuff,
         })
 
         console.log("onload")
-        this.timerId = this.startTimer()
     },
 
-    //baseincome * star_level
-    getAllBaseIncome:function(){
-        var result = {}
-        for(var id in self_configs.buildings){
-            result[id] = 0
-            var data = FooData.getBuildingData(id)
-            var cfg = self_configs.buildings[id]
-            result[id] = (data.income_offset * 0.01) * (FooData.star_offset[cfg.star_level] * 0.01)
-        }
-        return result
-    },
+    startWorker: function (page) {
+        console.log(page.setData)
+        var worker = wx.createWorker('workers/compute.js')
+        if (worker) {
+            worker.onMessage(function (res) {
+                // console.log(res)
 
-    //获得当前组合中的所有buff加成
-    getAllBuffInCombination:function(combination){
-        var result = {}
-        for(var id in combination){
-            const building = combination[id]
-            if(building.buff_type){
-                for(var index in building.buff_type){
-                    const buff_type = building.buff_type[index].bufftype
-                    const star_level = self_configs.buildings[building.id].star_level
-                    result[buff_type] += building.buff_type[index].value[star_level]
+                if (res.msg === "returnBestCombination") {
+                    page.setData({
+                        computing: false,
+                        compute_cost_time: res.data.cost_time
+                    })
+                } else if(res.msg === "returnComputProgress"){
+                    page.data.current_count = res.data.current_count
+                    page.data.max_count = res.data.max_count
                 }
+            })
+        }
+        return worker
+    },
+
+    stopWorker: function (worker) {
+        if (worker) {
+            worker.terminate()
+        }
+    },
+
+    startTimer: function (page) {
+        var timer = setInterval(() => {
+            page.setData({
+                current_count: page.data.current_count,
+                max_count: page.data.max_count
+            })
+            if(page.data.current_count >= page.data.max_count){
+                clearInterval(timer)
             }
-        }
-        return result
-    },
-
-    //建筑间加成,互相在阵容内才有加成
-    getBuildingBuff:function(building, combination, all_buff){
-        var result = 100
-        for(var i in combination){
-            const test = combination[i]
-            if(test.benefit_list){
-                for(var j in test.benefit_list){
-                    if(test.benefit_list[j].building_id == building.id){
-                        var offset = self_configs.buildings[building.id].star_level
-                        result += test.benefit_list[j].value[offset]
-                    }
-                }
-            }
-        }
-
-        for(var buff_type in building.affect_buffs){
-            if(buff_type == this.data.filter_buff_type){
-                continue;
-            }
-            result += all_buff[buff_type]
-        }
-        return result * 0.01 
-    },
-
-    getAllPolicyBuff:function(){
-        var result = {}
-        for(var i in FooData.buff_type){
-            var buff = FooData.buff_type[i]
-            result[buff.id] = 0
-        }
-    
-        for(var policy_id in self_configs.policies){
-            var policy = FooData.getPolicyData(policy_id)
-            var level = self_configs.policies[policy_id].level
-            result[policy.buff_type] += policy.enhance_value[level]
-        }
-
-        for(var buff_type in self_configs.extra_buff){
-            result[buff_type] += self_configs.extra_buff[buff_type].value
-        }
-        return result
-    },
-
-    getPolicyBuff:function(building){
-        var result = 100
-
-        for(var buff_type in building.affect_buffs){
-            if(buff_type == this.data.filter_buff_type){
-                continue;
-            }
-            result += this.allPolicyBuff[buff_type]
-        }
-
-        return result * 0.01
-    },
-
-    getAlbumBuff:function(building){
-        var result = 100
-
-        for(var buff_type in building.affect_buffs){
-            if(buff_type == this.data.filter_buff_type){
-                continue;
-            }
-            result += self_configs.albums[buff_type].value
-        }
-
-        return result * 0.01
-    },
-
-    getCityMissionBuff:function(building){
-        var result = 100
-        var city_building_enhance = self_configs.city_missions.building
-
-        for(var buff_type in building.affect_buffs){
-            if(buff_type == this.data.filter_buff_type){
-                continue;
-            }
-            result += self_configs.city_missions.buff[buff_type].value
-        }
-
-        for(var i in city_building_enhance){
-            if(city_building_enhance[i].id == building.id){
-                result += city_building_enhance[i].value
-            }
-        }
-        return result * 0.01
-    },
-
-    getOnlineOfflineFactor:function(online){
-        return online ? 1 : 0.5
-    },
-
-    //单建筑收益
-    //income = base_income * e_star * e_builiding * e_policy * e_album * e_cityMission 
-    getBuildingIncome:function(building_data, building_level, combination, all_buff){
-        return this.getOnlineOfflineFactor(this.data.online_flag)
-        * this.data.allBaseIncome[building_data.id]
-        * (FooData.level_income[building_level] * 0.01)
-        * this.getBuildingBuff(building_data, combination, all_buff) 
-        * this.getPolicyBuff(building_data)
-        * this.getAlbumBuff(building_data)
-        * this.getCityMissionBuff(building_data)
-    },
-
-    //获取单个建筑一定范围内的升级消耗比例
-    //升级消耗比(ratio of income increase to upgrade cost)
-    getBuildingRICArray:function(range, building_id, building_star_level, building_level, combination){
-        var result = []
-        var rare = FooData.getBuildingData(building_id).rare
-        for(var i = 0; i < range; i++){
-            var ia = this.getBuildingIncome(building_id, building_star_level, building_level+i+1, combination)
-            var ib = this.getBuildingIncome(building_id, building_star_level, building_level+i, combination)
-            var cost = FooData.getUpgradeCost(building_id, rare)
-            result.push((ia-ib)/cost)
-        }
-        return result
+        }, 100)
     },
 
     /**
@@ -234,7 +104,7 @@ Page({
      */
     onUnload: function () {
         console.log("unload")
-        clearInterval(this.timerId)
+        this.stopWorker(this.compute_worker)
     },
 
     /**
@@ -258,78 +128,25 @@ Page({
 
     },
 
-    onSwitch_OnlineFlag:function(event){
+    onSwitch_OnlineFlag: function (event) {
         this.setData({
             online_flag: event.detail.value,
-            filter_buff_type: event.detail.value ? 3:2,
+            filter_buff_type: event.detail.value ? 3 : 2,
         })
     },
 
-    startTimer:function(){
-        var page = this
-        return setInterval(()=>{
-            if(page.data.computing){
-                console.log("computing")
-                page.setData({
-                    current_count:page.data.current_count,
-                    max_count:page.data.max_count
-                }) 
+    onClick_StartCalc: function () {
+
+        this.compute_worker.postMessage({
+            msg: "getBestCombination",
+            data: {
+                online_flag: this.data.online_flag
             }
-        }, 100)
-    },
+        })
+        this.startTimer(this)
 
-    onClick_StartCalc:function(){
-        var icbn = utils.getCombinations(this.data.industry_buildings, 3)
-        var bcbn = utils.getCombinations(this.data.business_buildings, 3)
-        var rcbn = utils.getCombinations(this.data.residence_buildings, 3)
-
-        this.data.max_count = utils.combinationCount(this.data.industry_buildings.length, 3) 
-        * utils.combinationCount(this.data.business_buildings.length, 3) 
-        * utils.combinationCount(this.data.residence_buildings.length, 3)
-        this.data.current_count = 0
-        
-        var start_time = new Date().getTime()
         this.setData({
             computing: true,
-            current_count: this.data.current_count,
-            max_count: this.data.max_count,
-        })
-
-        //遍历所有组合
-        var mostIncome = 0
-        var resultCbn = []
-        for(var ii in icbn){
-            for(var ib in bcbn){
-                for(var ir in rcbn){
-                    this.data.current_count++
-                    var perhaps_cbn = [].concat(icbn[ii], bcbn[ib], rcbn[ir])
-                    var all_buff_in_cbn = this.getAllBuffInCombination(perhaps_cbn)
-                    var tmpIncome = 0
-                    for(var ip in perhaps_cbn){
-                        var level = self_configs.buildings[perhaps_cbn[ip].id].level
-                        tmpIncome += this.getBuildingIncome(perhaps_cbn[ip], level, perhaps_cbn, all_buff_in_cbn)
-                    } 
-                    if(tmpIncome > mostIncome){
-                        mostIncome = tmpIncome
-                        resultCbn = perhaps_cbn
-                    }
-                }
-            }
-        } 
-        // for(var ir in resultCbn){
-        //     var item = resultCbn[ir]
-        //     var star_level = self_configs.buildings[item.id].star_level
-        //     var level = self_configs.buildings[item.id].level
-        //     item.income = utils.numberFormat(this.getBuildingIncome(item.id, star_level, level, resultCbn)) 
-        //     //升级消耗比(ratio of income increase to upgrade cost)
-        //     item.ric = this.getBuildingRICArray(10, item.id, star_level, level, resultCbn)
-        // }
-        console.log(resultCbn)
-        console.log(utils.numberFormat(mostIncome))
-        this.setData({
-            computing: false,
-            current_count: this.data.current_count,
-            compute_cost_time:new Date().getTime() - start_time
         })
     }
 })
---
+++ workers/compute.js
@@ -0,0 +1,319 @@
+const utils = require('./utils.js')
+const Debug = true
+
+var FooData, self_configs = 'undefined'
+var industry_buildings, business_buildings, residence_buildings
+var allBaseIncome, allPolicyBuff
+//默认过滤离线buff
+const filter_buff_type = 3
+var online_flag = 1
+var current_count = 0
+var max_count = 0
+
+worker.onMessage(function (res) {
+    if (Debug) console.log(res)
+
+    if (res.msg === "getBestCombination") {
+        getBestCombination(res.data)
+    } else if (res.msg === "setData") {
+        setData(res.data)
+    }
+})
+
+const getBuildingData = function (building_id) {
+    for (var i in FooData.buildings_list) {
+        if (FooData.buildings_list[i].id == building_id) {
+            return FooData.buildings_list[i];
+        }
+    }
+    console.log("错误的建筑id");
+    return null;
+}
+
+const getPolicyData = function (policy_id) {
+    for (var stage in FooData.policy_list) {
+        var policy_stage = FooData.policy_list[stage]
+        for (var i in policy_stage) {
+            if (policy_stage[i].id == policy_id) {
+                return policy_stage[i]
+            }
+        }
+    }
+    console.log("错误的政策id");
+    return null;
+}
+
+//计算所有建筑的基本收益
+//baseincome * star_level
+const getAllBaseIncome = function () {
+    var result = {}
+    for (var id in self_configs.buildings) {
+        result[id] = 0
+        var data = getBuildingData(id)
+        var cfg = self_configs.buildings[id]
+        result[id] = (data.income_offset * 0.01) * (FooData.star_offset[cfg.star_level] * 0.01)
+    }
+    return result
+}
+
+//获得当前组合中的所有buff加成
+const getAllBuffInCombination = function (combination) {
+    var result = {}
+    for (var id in combination) {
+        const building = combination[id]
+        if (building.buff_type) {
+            for (var index in building.buff_type) {
+                const buff_type = building.buff_type[index].bufftype
+                const star_level = self_configs.buildings[building.id].star_level
+                result[buff_type] += building.buff_type[index].value[star_level]
+            }
+        }
+    }
+    return result
+}
+
+//建筑间加成,互相在阵容内才有加成
+const getBuildingBuff = function (building, combination, all_buff) {
+    var result = 100
+    for (var i in combination) {
+        const test = combination[i]
+        if (test.benefit_list) {
+            for (var j in test.benefit_list) {
+                if (test.benefit_list[j].building_id == building.id) {
+                    var offset = self_configs.buildings[building.id].star_level
+                    result += test.benefit_list[j].value[offset]
+                }
+            }
+        }
+    }
+
+    for (var buff_type in building.affect_buffs) {
+        if (buff_type == filter_buff_type) {
+            continue;
+        }
+        result += all_buff[buff_type]
+    }
+    return result * 0.01
+}
+
+//获得所有类型的政策buff加成
+const getAllPolicyBuff = function () {
+    var result = {}
+    for (var i in FooData.buff_type) {
+        var buff = FooData.buff_type[i]
+        result[buff.id] = 0
+    }
+
+    for (var policy_id in self_configs.policies) {
+        var policy = getPolicyData(policy_id)
+        var level = self_configs.policies[policy_id].level
+        result[policy.buff_type] += policy.enhance_value[level]
+    }
+
+    for (var buff_type in self_configs.extra_buff) {
+        result[buff_type] += self_configs.extra_buff[buff_type].value
+    }
+    return result
+}
+
+const getPolicyBuff = function (building) {
+    var result = 100
+
+    for (var buff_type in building.affect_buffs) {
+        if (buff_type == filter_buff_type) {
+            continue;
+        }
+        result += this.allPolicyBuff[buff_type]
+    }
+
+    return result * 0.01
+}
+
+const getAlbumBuff = function (building) {
+    var result = 100
+
+    for (var buff_type in building.affect_buffs) {
+        if (buff_type == filter_buff_type) {
+            continue;
+        }
+        result += self_configs.albums[buff_type].value
+    }
+
+    return result * 0.01
+}
+
+const getCityMissionBuff = function (building) {
+    var result = 100
+    var city_building_enhance = self_configs.city_missions.building
+
+    for (var buff_type in building.affect_buffs) {
+        if (buff_type == filter_buff_type) {
+            continue;
+        }
+        result += self_configs.city_missions.buff[buff_type].value
+    }
+
+    for (var i in city_building_enhance) {
+        if (city_building_enhance[i].id == building.id) {
+            result += city_building_enhance[i].value
+        }
+    }
+    return result * 0.01
+}
+
+const getOnlineOfflineFactor = function (online) {
+    return online ? 1 : 0.5
+}
+
+//单建筑收益
+//income = base_income * e_star * e_builiding * e_policy * e_album * e_cityMission 
+const getBuildingIncome = function (building_data, building_level, combination, all_buff) {
+    return getOnlineOfflineFactor(online_flag)
+        * allBaseIncome[building_data.id]
+        * (FooData.level_income[building_level] * 0.01)
+        * getBuildingBuff(building_data, combination, all_buff)
+        * getPolicyBuff(building_data)
+        * getAlbumBuff(building_data)
+        * getCityMissionBuff(building_data)
+}
+
+//获取单个建筑一定范围内的升级消耗比例
+//升级消耗比(ratio of income increase to upgrade cost)
+const getBuildingRICArray = function (range, building_id, building_star_level, building_level, combination) {
+    var result = []
+    var rare = getBuildingData(building_id).rare
+    for (var i = 0; i < range; i++) {
+        var ia = this.getBuildingIncome(building_id, building_star_level, building_level + i + 1, combination)
+        var ib = this.getBuildingIncome(building_id, building_star_level, building_level + i, combination)
+        var cost = FooData.getUpgradeCost(building_id, rare)
+        result.push((ia - ib) / cost)
+    }
+    return result
+}
+
+// data:{
+//     db  数据库
+//     self_configs 个人配置
+// }
+const setData = function (data) {
+    FooData = data.db
+    self_configs = data.self_configs
+    getBuildingList()
+    allBaseIncome = getAllBaseIncome()
+    allPolicyBuff = getAllPolicyBuff()
+}
+
+//获得建筑列表
+const getBuildingList = function () {
+    industry_buildings = []
+    business_buildings = []
+    residence_buildings = []
+    if (Debug) console.log(FooData)
+    for (var i in FooData.buildings_list) {
+        var building = FooData.buildings_list[i]
+        if (!self_configs.buildings[building.id].enabled) {
+            continue
+        }
+        if (building.type == 1) {
+            industry_buildings.push(building)
+        }
+        if (building.type == 2) {
+            business_buildings.push(building)
+        }
+        if (building.type == 3) {
+            residence_buildings.push(building)
+        }
+    }
+}
+
+// data:{
+//     online_flag
+// }
+const getBestCombination = function (data) {
+    if (Debug) console.log(FooData, self_configs)
+    online_flag = data.online_flag
+    var icbn = utils.getCombinations(industry_buildings, 3)
+    var bcbn = utils.getCombinations(business_buildings, 3)
+    var rcbn = utils.getCombinations(residence_buildings, 3)
+
+    //当前尝试数量
+    current_count = 0
+    max_count = icbn.length * bcbn.length * rcbn.length
+    worker.postMessage({
+        msg: "returnComputProgress",
+        data: {
+            current_count: current_count,
+            max_count: max_count
+        },
+    })
+
+    var start_time = new Date().getTime()
+    //遍历所有组合
+    var mostIncome = 0
+    var resultCbn = []
+    //发送进度的计数
+    var sendProgressIndex = 0
+    const sendProgressInterval = 100
+    for (var ii in icbn) {
+        for (var ib in bcbn) {
+            for (var ir in rcbn) {
+                current_count++
+                var perhaps_cbn = [].concat(icbn[ii], bcbn[ib], rcbn[ir])
+                var all_buff_in_cbn = getAllBuffInCombination(perhaps_cbn)
+                var tmpIncome = 0
+                for (var ip in perhaps_cbn) {
+                    var level = self_configs.buildings[perhaps_cbn[ip].id].level
+                    tmpIncome += getBuildingIncome(perhaps_cbn[ip], level, perhaps_cbn, all_buff_in_cbn)
+                }
+                if (tmpIncome > mostIncome) {
+                    mostIncome = tmpIncome
+                    resultCbn = perhaps_cbn
+                }
+
+                sendProgressIndex++
+                if(sendProgressIndex > sendProgressInterval || current_count >= max_count){
+                    sendProgressIndex = 0
+                    worker.postMessage({
+                        msg: "returnComputProgress",
+                        data: {
+                            current_count: current_count,
+                            max_count: max_count
+                        },
+                    })
+                }
+            }
+        }
+    }
+
+    var resultdata = {
+        // 最大收入/秒
+        mostIncome: utils.numberFormat(mostIncome),
+        // 最佳组合
+        resultCbn: resultCbn,
+        // 计算消耗时间
+        cost_time: new Date().getTime() - start_time,
+        // 所有建筑基本收入
+        allBaseIncome: allBaseIncome,
+        // 所有建筑buff加成
+        allPolicyBuff: allPolicyBuff,
+    }
+
+    if (Debug) console.log(resultdata)
+    // return result
+    worker.postMessage({
+        msg: "returnBestCombination",
+        data: resultdata,
+    })
+}
+
+
+const getRic = function (data) {
+    for (var ir in resultCbn) {
+        var item = resultCbn[ir]
+        var star_level = self_configs.buildings[item.id].star_level
+        var level = self_configs.buildings[item.id].level
+        item.income = utils.numberFormat(this.getBuildingIncome(item.id, star_level, level, resultCbn))
+        //升级消耗比(ratio of income increase to upgrade cost)
+        item.ric = this.getBuildingRICArray(10, item.id, star_level, level, resultCbn)
+    }
+}
